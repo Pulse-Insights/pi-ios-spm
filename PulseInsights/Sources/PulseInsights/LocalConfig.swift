eb9326f98d82e85deaf027f3ff668af910fb844e
shenlong <shenlong@ekohe.com>
2025-01-17 18:05:21 +0800

    MISC - Fix customData missing in request issue

--- PulseInsights/Sources/PulseInsights/LocalConfig.swift
+++ PulseInsights/Sources/PulseInsights/LocalConfig.swift
@@ -52,7 +52,6 @@ class LocalConfig {
         pollResults = []
         strSubmitID = ""
         strCheckingSurveyID = ""
-        customData = [:]
         inlineLink = [:]
         surveyWatcherEnable = false
     }
