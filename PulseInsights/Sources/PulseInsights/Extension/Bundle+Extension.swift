a52b82e8a85ac614e2205e12255dbf122fd4a370
shenlong <shenlong@ekohe.com>
2025-06-03 17:05:43 +0800

    Fix cocoapods bundle issue

--- PulseInsights/Sources/PulseInsights/Extension/Bundle+Extension.swift
+++ PulseInsights/Sources/PulseInsights/Extension/Bundle+Extension.swift
@@ -10,20 +10,19 @@ import Foundation
 
 extension Bundle {
     static var pulseInsightsBundle: Bundle {
-        // For SPM, try to use Bundle.module
         #if SWIFT_PACKAGE
         return Bundle.module
         #else
-        // For CocoaPods, use the bundle for PulseInsights class
-        return Bundle(for: PulseInsights.self)
+        let frameworkBundle = Bundle(for: PulseInsights.self)
+        if let resourceBundleURL = frameworkBundle.url(forResource: "PulseInsightsSPM", withExtension: "bundle"),
+           let resourceBundle = Bundle(url: resourceBundleURL) {
+            return resourceBundle
+        }
+        if let resourceBundleURL = Bundle.main.url(forResource: "PulseInsightsSPM", withExtension: "bundle"),
+           let resourceBundle = Bundle(url: resourceBundleURL) {
+            return resourceBundle
+        }
+        return frameworkBundle
         #endif
     }
-    
-    static func pulseInsightsBundle(for aClass: AnyClass) -> Bundle {
-        #if SWIFT_PACKAGE
-        return Bundle.module
-        #else
-        return Bundle(for: aClass)
-        #endif
-    }
-} 
+}
